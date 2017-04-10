b99505cda9216422a34ad8dcfb7fd9670f11acc4
Dmitry Meshkov <demansong@gmail.com>
2017-04-11 01:17:16 +0300

    Added models functions

--- main.cpp
+++ main.cpp
@@ -140,6 +140,146 @@ vector<pair<boost::gregorian::date,double> > get_min_max_for_kernel_density(cons
     return res;
 }
 
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_hs_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=1;i<min_maxes.size()-4;i+=2)
+    {
+        if(!(min_maxes[i+2].second>min_maxes[i].second&&min_maxes[i+2].second>min_maxes[i+4].second)) continue;
+        double max_avg=(min_maxes[i].second+min_maxes[i+4].second)/2;
+        double min_avg=(min_maxes[i+1].second+min_maxes[i+3].second)/2;
+        if(!(abs(min_maxes[i].second-max_avg)<=0.015*max_avg&&abs(min_maxes[i+4].second-max_avg)<=0.015*max_avg)) continue;
+        if(!(abs(min_maxes[i+1].second-min_avg)<=0.015*min_avg&&abs(min_maxes[i+3].second-min_avg)<=0.015*min_avg)) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_ihs_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=((!min_maxes[0].second)?2:0);i<min_maxes.size()-4;i+=2)
+    {
+        if(!(min_maxes[i+2].second<min_maxes[i].second&&min_maxes[i+2].second<min_maxes[i+4].second)) continue;
+        double min_avg=(min_maxes[i].second+min_maxes[i+4].second)/2;
+        double max_avg=(min_maxes[i+1].second+min_maxes[i+3].second)/2;
+        if(!(abs(min_maxes[i].second-min_avg)<=0.015*min_avg&&abs(min_maxes[i+4].second-min_avg)<=0.015*min_avg)) continue;
+        if(!(abs(min_maxes[i+1].second-max_avg)<=0.015*max_avg&&abs(min_maxes[i+3].second-max_avg)<=0.015*max_avg)) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_btop_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=1;i<min_maxes.size()-4;i+=2)
+    {
+        if(!(min_maxes[i].second<min_maxes[i+2].second&&min_maxes[i+2].second<min_maxes[i+4].second)) continue;
+        if(!(min_maxes[i+1].second>min_maxes[i+3].second)) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_bbot_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=((!min_maxes[0].second)?2:0);i<min_maxes.size()-4;i+=2)
+    {
+        if(!(min_maxes[i].second>min_maxes[i+2].second&&min_maxes[i+2].second>min_maxes[i+4].second)) continue;
+        if(!(min_maxes[i+1].second<min_maxes[i+3].second)) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_ttop_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=1;i<min_maxes.size()-4;i+=2)
+    {
+        if(!(min_maxes[i].second>min_maxes[i+2].second&&min_maxes[i+2].second>min_maxes[i+4].second)) continue;
+        if(!(min_maxes[i+1].second<min_maxes[i+3].second)) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_tbot_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=((!min_maxes[0].second)?2:0);i<min_maxes.size()-4;i+=2)
+    {
+        if(!(min_maxes[i].second<min_maxes[i+2].second&&min_maxes[i+2].second<min_maxes[i+4].second)) continue;
+        if(!(min_maxes[i+1].second>min_maxes[i+3].second)) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_rtop_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=1;i<min_maxes.size()-4;i+=2)
+    {
+        double max_avg=(min_maxes[i+2].second+min_maxes[i+4].second)/2;
+        double min_avg=(min_maxes[i+1].second+min_maxes[i+3].second)/2;
+        if(!(abs(min_maxes[i+2].second-max_avg)<=0.0075*max_avg&&abs(min_maxes[i+4].second-max_avg)<=0.0075*max_avg)) continue;
+        if(!(abs(min_maxes[i+1].second-min_avg)<=0.0075*min_avg&&abs(min_maxes[i+3].second-min_avg)<=0.0075*min_avg)) continue;
+        if(!(min({min_maxes[i].second,min_maxes[i+2].second,min_maxes[i+4].second})>max({min_maxes[i+1].second,min_maxes[i+3].second}))) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_rbot_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=((!min_maxes[0].second)?2:0);i<min_maxes.size()-4;i+=2)
+    {
+        double min_avg=(min_maxes[i+2].second+min_maxes[i+4].second)/2;
+        double max_avg=(min_maxes[i+1].second+min_maxes[i+3].second)/2;
+        if(!(abs(min_maxes[i+2].second-min_avg)<=0.0075*min_avg&&abs(min_maxes[i+4].second-min_avg)<=0.0075*min_avg)) continue;
+        if(!(abs(min_maxes[i+1].second-max_avg)<=0.0075*max_avg&&abs(min_maxes[i+3].second-max_avg)<=0.0075*max_avg)) continue;
+        if(!(max({min_maxes[i].second,min_maxes[i+2].second,min_maxes[i+4].second})<min({min_maxes[i+1].second,min_maxes[i+3].second}))) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_maxes[i+4].first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_dtop_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=1;i<min_maxes.size()-2;i+=2)
+    {
+        pair<boost::gregorian::date,double> max_next={boost::gregorian::date(),DBL_MIN};
+        for(size_t j=i+2;j<min_maxes.size()-1;j+=2)
+            if(min_maxes[j].second>max_next.second) max_next=min_maxes[j];
+        double max_avg=(min_maxes[i].second+max_next.second)/2;
+        if(!(abs(min_maxes[i].second-max_avg)<=0.015*max_avg&&abs(max_next.second-max_avg)<=0.015*max_avg)) continue;
+        if(!(max_next.first-min_maxes[i].first>boost::gregorian::date_duration(22))) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,max_next.first));
+    }
+    return dates;
+}
+
+vector<pair<boost::gregorian::date,boost::gregorian::date> > get_dbot_models(const vector<pair<boost::gregorian::date,double> >& min_maxes)
+{
+    vector<pair<boost::gregorian::date,boost::gregorian::date> > dates;
+    for(size_t i=((!min_maxes[0].second)?2:0);i<min_maxes.size()-2;i+=2)
+    {
+        pair<boost::gregorian::date,double> min_next={boost::gregorian::date(),DBL_MAX};
+        for(size_t j=i+2;j<min_maxes.size()-1;j+=2)
+            if(min_maxes[j].second<min_next.second) min_next=min_maxes[j];
+        double min_avg=(min_maxes[i].second+min_next.second)/2;
+        if(!(abs(min_maxes[i].second-min_avg)<=0.015*min_avg&&abs(min_next.second-min_avg)<=0.015*min_avg)) continue;
+        if(!(min_next.first-min_maxes[i].first>boost::gregorian::date_duration(22))) continue;
+        dates.emplace_back(make_pair(min_maxes[i].first,min_next.first));
+    }
+    return dates;
+}
+
 int main()
 {
     map<string,vector<pair<boost::gregorian::date,double> > > values;
@@ -185,6 +325,8 @@ int main()
         for(auto& mm:min_maxs_k) cout << mm.first << " : " << mm.second << '\n';
         cout << '\n';
     }
+
+    //часть,нужная для заполнения res.csv. Для ее активации надо закомментить часть от values.erase("") до нее, раскомментить эту часть, а также строки, связанный с ofstring out. Время выполнения перезаписи ~1 часа, в зависимости от количества ядер процессора.
     /*
     out << "Company;Alpha;h*\n";
     for(auto& el:values)
